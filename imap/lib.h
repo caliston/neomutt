16a6c6edbe166e93ccb52469ba2dffed8cefd118
Richard Russon <rich@flatcap.org>
2020-02-21 01:10:41 +0000

    path: imap

--- imap/lib.h
+++ imap/lib.h
@@ -41,6 +41,7 @@
  * | imap/browse.c     | @subpage imap_browse     |
  * | imap/command.c    | @subpage imap_command    |
  * | imap/message.c    | @subpage imap_message    |
+ * | imap/path.c       | @subpage imap_path       |
  * | imap/utf7.c       | @subpage imap_utf7       |
  * | imap/util.c       | @subpage imap_util       |
  */
@@ -54,11 +55,13 @@
 #include <sys/types.h>
 #include "core/lib.h"
 #include "mx.h"
+#include "path.h"
 
 struct BrowserState;
 struct Buffer;
 struct ConnAccount;
 struct EmailList;
+struct Path;
 struct PatternList;
 struct stat;
 
